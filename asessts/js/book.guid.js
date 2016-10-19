700630ae39c61bde4fcc11c5011e509a2d294b75
huanlv <huanlv@topica.edu.vn>
2016-10-19 19:38:56 +0700

    fix js

--- asessts/js/book.guid.js
+++ asessts/js/book.guid.js
@@ -114,24 +114,18 @@ $(function() {
   //   }
   // });
 
-
-
-
-
   $('#guid-book').turn({
     width: 992,
     height: 680,
     when: {
       turned: function(event, page, pageObj) {
-        console.log(event.type);
-
-        if (page == 2) {
+        if (page == 4) {
           $('.content-text-page-2').show('slide', { direction: "left" }, 1000);
           $('.sub-text-page-2').show('slide', { direction: "right" }, 1000);
           $('.text-left-page-3').show('slide', { direction: 'left' }, 1000);
           $('.text-right-page-3').show('slide', { direction: 'right' }, 1000);
         }
-        else if (page == 4) {
+        else if (page == 6) {
           $('.text-intro-page-2').show('slide', { direction: "left" }, 1000);
           $('.sub-text-intro-page-2').show('slide', { direction: "right" }, 1000);
           $('.div-left-page-6').show('slide', { direction: "left" }, 1000);
@@ -139,25 +133,25 @@ $(function() {
           $('.tlt-mid-page-4').show('drop', { direction: "down" }, 1000);
           $('.img-page-4').show('drop', { direction: "down" }, 1000)
         }
-        else if (page == 6) {
+        else if (page == 8) {
           $('.div-1-page-8').show('drop', { direction: "down" }, 600);
           $('.div-2-page-8').show('drop', { direction: "down" }, 1200);
           $('.div-3-page-8').show('drop', { direction: "down" }, 1800);
           $('.div-4-page-8').show('drop', { direction: "down" }, 2400);
           $('.div-5-page-8').show('drop', { direction: "down" }, 3000);
         }
-        else if (page == 8) {
+        else if (page == 10) {
           $('.left-page-10').show('slide', { direction: "left" }, 1000);
           $('.right-page-10').show('slide', { direction: "right" }, 1000);
         }
-        else if (page == 12) {
+        else if (page == 14) {
           $('.titlte-page-10').show('drop', { direction: "left" }, 1000);
           $('.content-page-10').show('drop', { direction: "left" }, 1000);
           $('.list-page-10').show('drop', { direction: "left" }, 1000);
           $('.img-page-14').show('drop', { direction: "left" }, 1000);
           $('.list-page-14').show('drop', { direction: "right" }, 1000);
         }
-        else if (page == 14) {
+        else if (page == 16) {
           $('.img-1-page-15').show('drop', { direction: "down" }, 400);
           $('.img-2-page-15').show('drop', { direction: "down" }, 600);
           $('.text-1-page-15').show('drop', { direction: "down" }, 800);
@@ -167,7 +161,7 @@ $(function() {
           $('.right-page-16').show('drop', { direction: "right" }, 1000);
           $('.img-page-16').show('drop', { direction: "down" }, 1000);
         }
-        else if (page == 16) {
+        else if (page == 18) {
           $('.modal-page-17').show('drop', { direction: "right" }, 800);
           $('.text-1-page-17').show('drop', { direction: "down" }, 1000);
           $('.list-page-17-1').show('drop', { direction: "left" }, 1000);
@@ -176,7 +170,7 @@ $(function() {
           $('.right-page-18').show('drop', { direction: "right" }, 1000);
           $('.img-page-18').show('drop', { direction: "down" }, 1000)
         }
-        else if (page == 18) {
+        else if (page == 20) {
           $('.img-1-page-19').show('drop', { direction: "down" }, 800);
           $('.img-2-page-19').show('drop', { direction: "down" }, 1000);
           $('.text-1-page-19').show('drop', { direction: "down" }, 1000);
@@ -187,7 +181,7 @@ $(function() {
           $('.list-page-20').show('drop', { direction: "right" }, 1000);
           $('.img-page-20').show('drop', { direction: "down" }, 1000);
         }
-        else if (page == 20) {
+        else if (page == 22) {
           $('.text-content-page-21').show('drop', { direction: "down" }, 800);
           $('.list-page-21').show('drop', { direction: "left" }, 1000);
           $('.div-right-page-21').show('drop', { direction: "right" }, 1000);
@@ -195,21 +189,25 @@ $(function() {
           $('.list-page-22').show('drop', { direction: "right" }, 1000);
           $('.div-left-page-22').show('drop', { direction: "left" }, 1000);
         }
-        else if (page == 24) {
+        else if (page == 26) {
           $('.text-intro-page-26').show('drop', { direction: "right" }, 1000);
           $('.sub-text-intro-page-26').show('drop', { direction: "right" }, 1000);
         }
-        else if (page == 26) {
+        else if (page == 28) {
           $('.text-left-page-27').show('drop', { direction: "left" }, 800);
           $('.text-right-page-27').show('drop', { direction: "right" }, 1000);
           $('.content-text-page-28').show('drop', { direction: "down" }, 1000);
         }
-        else if (page == 28) {
+        else if (page == 30) {
           $('.text-page-29').show('drop', { direction: "left" }, 1000);
           $('.page-30-left').show('drop', { direction: "left" }, 1000);
           $('.page-30-right').show('drop', { direction: "right" }, 1000);
         }
       }
     }
+  });
+
+  $("#guid-book").bind("turned", function(event, page, view) {
+    // console.log(page);
   });
 });
